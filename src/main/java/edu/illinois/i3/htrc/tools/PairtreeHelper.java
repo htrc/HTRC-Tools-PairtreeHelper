a60e2e11eadc43bfea6569c3cb51d25df2660c7e
Boris Capitanu <borice@hotmail.com>
2015-07-27 20:01:40 +0300

    Added more flexibility to use as a standalone tool (via command line parameters)

--- src/main/java/edu/illinois/i3/htrc/tools/PairtreeHelper.java
+++ src/main/java/edu/illinois/i3/htrc/tools/PairtreeHelper.java
@@ -1,10 +1,14 @@
 package edu.illinois.i3.htrc.tools;
 
+import com.beust.jcommander.*;
 import gov.loc.repository.pairtree.Pairtree;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -213,66 +217,200 @@ public class PairtreeHelper {
         }
     }
 
+    private static final String APP_NAME = System.getProperty("app.name", PairtreeHelper.class.getSimpleName());
+
     public static void main(String[] args) {
-        final String NO_HEADER_ARG = "--no-header";
+        PairtreeHelperCommands commands = new PairtreeHelperCommands();
+        JCommander commander = new JCommander(commands);
+        commander.setProgramName(APP_NAME);
 
-        if (args.length < 1 || args.length > 2)
-            showUsageAndExit();
+        commander.addCommand("clean", commands.cmdCleanId);
+        commander.addCommand("unclean", commands.cmdUncleanId);
+        commander.addCommand("clean2pt", commands.cmdGetPathFromCleanId);
+        commander.addCommand("unclean2pt", commands.cmdGetPathFromUncleanId);
+        commander.addCommand("parse", commands.cmdParse);
 
-        boolean showHeader = true;
-        String filePath = null;
-
-        if (args.length == 2 && args[1].equals(NO_HEADER_ARG)) {
-            showHeader = false;
-            filePath = args[0];
-        }
-
-        else
-
-        if (args.length == 2 && args[0].equals(NO_HEADER_ARG)) {
-            showHeader = false;
-            filePath = args[1];
-        }
-
-        else
-
-        if (args.length == 1) {
-            if (args[0].equals(NO_HEADER_ARG))
-                showUsageAndExit();
-
-            filePath = args[0];
-        }
-
-        if (filePath == null)
-            showUsageAndExit();
+        if (args.length == 0)
+            args = new String[] { "--help" };
 
         try {
-            File file = new File(filePath);
-            PairtreeDocument document = parse(file);
-            String uncleanId = document.getUncleanId();
-            String cleanId = document.getCleanId();
-            String libraryId = document.getLibraryId();
-            String name = file.getName();
+            commander.parse(args);
 
-            if (showHeader) {
-                String fmtUncleanId = String.format("%%-%ds", uncleanId.length());
-                String fmtCleanId = String.format("%%-%ds", cleanId.length());
-                String fmtLibId = String.format("%%-%ds", libraryId.length());
-                String fmtName = String.format("%%-%ds", name.length());
-                String fmt = String.format("%s %s %s %s", fmtUncleanId, fmtCleanId, fmtLibId, fmtName);
-                System.out.println(String.format(fmt, "uncleanId", "cleanId", "lib", "file"));
+            if (commands.cmdHelp.help) {
+                commander.usage();
+                System.exit(-1);
+            }
+        }
+        catch (ParameterException e) {
+            commander.usage();
+            System.exit(-2);
+        }
+
+        String command = commander.getParsedCommand();
+
+        if ("clean".equalsIgnoreCase(command)) {
+            List<String> uncleanIds = commands.cmdCleanId.uncleanIds;
+            if (uncleanIds == null) {
+                uncleanIds = new ArrayList<>();
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext())
+                    uncleanIds.add(scanner.next());
+            }
+            for (String uncleanId : uncleanIds) {
+                int pos = uncleanId.indexOf(".");
+                String libId = "";
+                if (pos > 0) {
+                    libId = uncleanId.substring(0, pos+1);
+                    uncleanId = uncleanId.substring(pos+1);
+                }
+                System.out.println(libId + cleanId(uncleanId));
+            }
+        }
+
+        else
+
+        if ("unclean".equalsIgnoreCase(command)) {
+            List<String> cleanIds = commands.cmdUncleanId.cleanIds;
+            if (cleanIds == null) {
+                cleanIds = new ArrayList<>();
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext())
+                    cleanIds.add(scanner.next());
+            }
+            for (String cleanId : cleanIds) {
+                int pos = cleanId.indexOf(".");
+                String libId = "";
+                if (pos > 0) {
+                    libId = cleanId.substring(0, pos+1);
+                    cleanId = cleanId.substring(pos+1);
+                }
+                System.out.println(libId + uncleanId(cleanId));
+            }
+        }
+
+        else
+
+        if ("clean2pt".equalsIgnoreCase(command)) {
+            List<String> cleanIds = commands.cmdGetPathFromCleanId.cleanIds;
+            if (cleanIds == null) {
+                cleanIds = new ArrayList<>();
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext())
+                    cleanIds.add(scanner.next());
+            }
+            for (String cleanId : cleanIds) {
+                try {
+                    System.out.println(getPathFromCleanId(cleanId));
+                }
+                catch (InvalidHtrcIdException e) {
+                    System.err.println("Invalid HTRC id: " + cleanId);
+                }
+            }
+        }
+
+        else
+
+        if ("unclean2pt".equalsIgnoreCase(command)) {
+            List<String> uncleanIds = commands.cmdGetPathFromUncleanId.uncleanIds;
+            if (uncleanIds == null) {
+                uncleanIds = new ArrayList<>();
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext())
+                    uncleanIds.add(scanner.next());
+            }
+            for (String uncleanId : uncleanIds) {
+                try {
+                    System.out.println(getPathFromUncleanId(uncleanId));
+                }
+                catch (InvalidHtrcIdException e) {
+                    System.err.println("Invalid HTRC id: " + uncleanId);
+                }
+            }
+        }
+
+        else
+
+        if ("parse".equalsIgnoreCase(command)) {
+            List<String> paths = commands.cmdParse.paths;
+            if (paths == null) {
+                paths = new ArrayList<>();
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNext())
+                    paths.add(scanner.next());
             }
 
-            System.out.println(String.format("%s %s %s %s", uncleanId, cleanId, libraryId, name));
-        }
-        catch (IOException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
+            boolean showHeader = commands.cmdParse.showHeader;
+
+            if (showHeader)
+                System.out.println(String.format("%s\t%s\t%s\t%s", "uncleanId", "cleanId", "lib", "file"));
+
+            for (String filePath : paths) {
+                try {
+                    File file = new File(filePath);
+                    PairtreeDocument document = parse(file);
+                    String uncleanId = document.getUncleanId();
+                    String cleanId = document.getCleanId();
+                    String libraryId = document.getLibraryId();
+                    String name = file.getName();
+
+                    System.out.println(String.format("%s\t%s\t%s\t%s", uncleanId, cleanId, libraryId, name));
+                }
+                catch (IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
         }
     }
 
-    private static void showUsageAndExit() {
-        System.out.println(String.format("Usage: %s <pairtree_file> [--no-header]", PairtreeHelper.class.getSimpleName()));
-        System.exit(-1);
+    public static class PairtreeHelperCommands {
+        @ParametersDelegate
+        public final CommandHelp cmdHelp = new CommandHelp();
+        public final CommandCleanId cmdCleanId = new CommandCleanId();
+        public final CommandUncleanId cmdUncleanId = new CommandUncleanId();
+        public final CommandGetPathFromCleanId cmdGetPathFromCleanId = new CommandGetPathFromCleanId();
+        public final CommandGetPathFromUncleanId cmdGetPathFromUncleanId = new CommandGetPathFromUncleanId();
+        public final CommandParse cmdParse = new CommandParse();
+    }
+
+    @Parameters(commandDescription = "Converts unclean ids into clean ids (that can be used as filenames)")
+    public static class CommandCleanId {
+        @Parameter(description = "<list-of-unclean-ids-to-clean>")
+        private List<String> uncleanIds;
+    }
+
+    @Parameters(commandDescription = "Converts clean ids back to the original unclean ids")
+    public static class CommandUncleanId {
+        @Parameter(description = "<list-of-clean-ids-to-convert>")
+        private List<String> cleanIds;
+    }
+
+    @Parameters(commandDescription = "Constructs the pairtree path associated with the given HTRC clean ids")
+    public static class CommandGetPathFromCleanId {
+        @Parameter(description = "<list-of-htrc-clean-ids>")
+        private List<String> cleanIds;
+    }
+
+    @Parameters(commandDescription = "Constructs the pairtree path associated with the given HTRC (unclean) ids")
+    public static class CommandGetPathFromUncleanId {
+        @Parameter(description = "<list-of-htrc-unclean-ids>")
+        private List<String> uncleanIds;
+    }
+
+    @Parameters(commandDescription = "Parses a pairtree path and reports the components of the path")
+    public static class CommandParse {
+        @Parameter(description = "<pairtree-paths>")
+        private List<String> paths;
+
+        @Parameter(names = "--header", description = "Specifies whether a header is included in the output (useful for CSV readers)")
+        private boolean showHeader = true;
+    }
+
+    public static class CommandHelp {
+        @Parameter(
+                names = { "-h", "-?", "-help", "--help" },
+                description = "Show help menu",
+                help = true
+        )
+        public boolean help;
     }
 }
